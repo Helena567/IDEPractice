f386e08b66000bccfdeee9a65bc01506abceb9e3
anushajoseph <anushajoseph@localhost>
2023-09-05 10:43:34 -0400

    Put Request updated

---
+++ RestAssured_Practice/src/test/java/com/lao/reqres/PutRequest.java
@@ -0,0 +1,35 @@
+package com.lao.reqres;
+
+import org.json.simple.JSONObject;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+
+public class PutRequest {
+	
+	@Test
+	public void putRequest() {
+		
+		 JSONObject jsonObject = new JSONObject();
+
+	        jsonObject.put("name", "Agni");
+	        jsonObject.put("job", "President");
+
+	        System.out.println(jsonObject.toString());
+
+	        RestAssured.baseURI="https://reqres.in";
+
+
+	        RestAssured
+	                .given()
+	                .header("", "")
+	                .body(jsonObject.toJSONString())
+	                .when()
+	                .post("/api/users")
+	                .then()
+	                .statusCode(200);
+
+		
+	}
+
+}
